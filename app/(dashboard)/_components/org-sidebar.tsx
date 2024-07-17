d1133813cfa9e5be5676bbddf0e66c30b15a4fff
parshav92 <dedhiaparshav@gmail.com>
2024-07-18 01:21:56 +0530

    orgSidebar and Navbar complete

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -6,12 +6,15 @@ import { cn } from '@/lib/utils';
 import { OrganizationSwitcher } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { LayoutDashboard, Star } from "lucide-react";
+import { useSearchParams } from "next/navigation";
 
 const font = Poppins({ subsets: ["latin"],
     weight: [ "600"], 
  });
 
 export const OrgSidebar = () => {
+    const searchParams = useSearchParams();
+    const favorites = searchParams.get("favorites");
     return (
 
             <div className="hidden lg:flex flex-col space-y-6 pl-5 pt-5 w-[206px] ">
@@ -45,17 +48,17 @@ export const OrgSidebar = () => {
                 />
 
                 <div className="flex flex-col space-y-4">
-                        <Button variant="ghost" asChild size="lg" className="font-normal justify-start px-2 w-full">
+                        <Button variant={favorites? "ghost" : "secondary"} asChild size="lg" className="font-normal justify-start px-2 w-full">
                     <Link href="/">
                             <LayoutDashboard className="h-4 w-4 mr-2" />
                             Team Boards
                     </Link>
                         </Button>
 
-                        <Button variant="ghost" asChild size="lg" className="font-normal justify-start px-2 w-full">
+                        <Button variant={favorites? "secondary" : "ghost"} asChild size="lg" className="font-normal justify-start px-2 w-full">
                     <Link href={{
                         pathname: "/",
-                        query: { favorite: true },
+                        query: { favorites: true },
                     }}>
                             <Star className="h-4 w-4 mr-2" />
                             Favorite Boards
--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,6 @@ export default clerkMiddleware((auth, req) => {
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)", ],
+  
 };
